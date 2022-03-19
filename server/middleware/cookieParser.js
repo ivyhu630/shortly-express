d5880587ab11ef3721e9769d58a9cee88c0ce574
ivyhu <ajianouc@gmail.com>
2022-03-18 17:05:17 -0700

    completed cookieParser

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,20 @@
 const parseCookies = (req, res, next) => {
+  var cookies = req.headers.cookie;
+  console.log(cookies);
+  if (cookies === undefined) {
+    req.cookies = {};
+  } else {
+    cookies = cookies.split(/[; =]+/);
+    console.log(cookies);
+    var n = cookies.length;
+    req.cookies = {};
+    for (var i = 0; i < n; i = i + 2) {
+      var key = cookies[i];
+      var value = cookies[i + 1];
+      req.cookies[key] = value;
+    }
+  }
+  next();
 };
 
 module.exports = parseCookies;
